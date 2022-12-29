b977c78058b476fdf2b6a2da8d79fd72fcaff5c0
leandrosuy <leandrosuy@gmail.com>
2022-12-29 15:02:32 -0300

    first commit

---
+++ src/data/questions_complete.js
@@ -0,0 +1,76 @@
+const data = [
+  {
+    category: "Português",
+    questions: [
+      {
+        question: "O emprego da vírgula tem função de separar orações em:",
+        options: [
+          "“A ideia de nós, os humanos, nos descolarmos da terra”",
+          "“Ela suprime a diversidade, nega a pluralidade das formas de vida”",
+          "“Oferece o mesmo cardápio, o mesmo figurino”",
+          "“Para a Unesco, 2019 foi o ano internacional das línguas indígenas”",
+        ],
+        answer:
+          "“Ela suprime a diversidade, nega a pluralidade das formas de vida”",
+        tip: "Aqui fica uma dica",
+      },
+      {
+        question:
+          "Reformulando o trecho “inspirar nossas escolhas”, o pronome encontra-se corretamente empregado em:",
+        options: [
+          "lhes inspirar",
+          "nos inspirar",
+          "inspirá-las",
+          "inspirar-lhe",
+        ],
+        answer: "inspirá-las",
+        tip: "Aqui fica uma dica",
+      },
+      {
+        question: "A palavra acentuada por ser uma proparoxítona é:",
+        options: ["possível", "indígenas", "sustentável", "também"],
+        answer: "indígenas",
+        tip: "Aqui fica uma dica",
+      },
+    ],
+  },
+  {
+    category: "Administração Geral",
+    questions: [
+      {
+        question:
+          "Suponha que determinada entidade pública pretenda realizar diagnóstico institucional e planejamento estratégico adotando como metodologia o Balanced Scorecard (BSC). Considerando os contornos de tal metodologia, é certo que ",
+        options: [
+          "pressupõe-se a prévia identificação da missão da organização, que corresponde ao futuro almejado por seus integrantes.",
+          "a organização precisará construir uma matriz SWOT, que constitui uma representação gráfica das metas e indicadores de desempenho adotados.",
+          "dependerá da presença de um consultor externo especializado que deverá aplicar o teste de Pareto para identificar o grau de maturidade da organização.",
+          "deverão ser consideradas, além das perspectiva financeira, também a dos clientes, a dos processos internos e a do aprendizado e crescimento.",
+          "a resultante obtida será o mapa estratégico da organização, no qual serão indicadas suas forças, fraquezas e as oportunidades e ameaças existentes.",
+        ],
+        answer:
+          "deverão ser consideradas, além das perspectiva financeira, também a dos clientes, a dos processos internos e a do aprendizado e crescimento.",
+        tip: "Aqui fica uma dica",
+      },
+      {
+        question:
+          "O Ciclo PDCA, também conhecido como Ciclo de Deming, que teve origem nos anos de 1950 e forte aplicação no Japão, consiste em um(a)",
+        options: [
+          "método de gerenciamento de projetos estratégicos, utilizado desde a etapa de desenvolvimento, concepção e aplicação.",
+          "metodologia de planejamento estratégico, que identifica os potenciais desafios e os caminhos para atingimento dos objetivos propostos.",
+          "diagrama para mapeamento dos principais processos da organização, com classificação nos quadrantes: prioritário; desnecessário; controlado; e adaptado.",
+          "processo de treinamento de pessoal voltado a aumentar o engajamento dos empregados utilizando os pilares: poder; desempenho; capacidade; e ação.",
+          "ferramenta utilizada para otimização de processos, incluindo as etapas de planejamento; execução; controle ou verificação; e ação corretiva.",
+        ],
+        answer:
+          "ferramenta utilizada para otimização de processos, incluindo as etapas de planejamento; execução; controle ou verificação; e ação corretiva.",
+      },
+      {
+        question: "A posição que deixa um elemento fixo é a:",
+        options: ["static", "absolute", "fixed", "relative"],
+        answer: "fixed",
+      },
+    ],
+  },
+];
+
+export default data;
